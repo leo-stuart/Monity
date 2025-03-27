a5d9da88284aeb0fe1abf7cd20ad358230a29cd7
Leonardo Stuart <leostuart05@gmail.com>
2025-03-27 20:53:00 -0300

    More features in main

--- develop/main.c
+++ develop/main.c
@@ -4,16 +4,17 @@
 
 int main()
 {
-    //Struct variable
+    // Struct variable
     Expense expense;
     int choice;
     do
     {
         printf("\n\n1. Add Expense\n");
         printf("2. List All Expenses\n");
-        printf("3. Exit\n");
+        printf("3. Filter Expenses\n");
+        printf("4. Exit\n");
 
-        printf("\nWhat do you want to do? [1 to 3]: ");
+        printf("\nWhat do you want to do? [1 to 4]: ");
         scanf("%i", &choice);
 
         switch (choice)
@@ -22,10 +23,36 @@ int main()
             add_expense(&expense);
             write(expense);
             break;
+
         case 2:
             list_expenses();
             break;
+
         case 3:
+            printf("\nHow do you want to filter?\n");
+            printf("\n1. Filter by Category\n");
+            printf("2. Filter by Date\n");
+
+            printf("\n[1|2]: ");
+            int filter_opt;
+            scanf("%i", &filter_opt);
+
+            switch (filter_opt)
+            {
+            case 1:
+                filter_by_cat();
+                break;
+
+            case 2:
+                filter_by_date();
+                break;
+
+            default:
+                break;
+            }
+            break;
+
+        case 4:
             return 0;
         default:
             break;
